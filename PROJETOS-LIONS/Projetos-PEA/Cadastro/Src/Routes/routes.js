91b690285e5b8a5d33cb9185f2776f8f9063ee7b
Filipe Paixão <fpedrapaixao@gmail.com>
2025-10-20 11:26:58 -0300

    Add login and profile routes to the user authentication system

--- PROJETOS-LIONS/Projetos-PEA/Cadastro/Src/Routes/routes.js
+++ PROJETOS-LIONS/Projetos-PEA/Cadastro/Src/Routes/routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { AddUser } = require("../Functions/AddUser");
 const { RemoveUser } = require("../Functions/RemoveUser");
+const { loginUser } = require("../Functions/LoginUser");
 
 const route = express.Router();
 
 route.post("/Cadastro", AddUser)
 route.delete("/Cadastro/:id", RemoveUser)
+route.post("/login", loginUser)
+route.get("/profile",authMiddleware, HelloUser )
 
 module.exports = route;
